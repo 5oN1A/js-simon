b97255dc8de1b6a94e6033a59784e1be1f4475d2
5oN1A <82112240+5oN1A@users.noreply.github.com>
2021-10-28 08:51:00 +0200

    final alert complete

--- js/script.js
+++ js/script.js
@@ -4,13 +4,13 @@ function randomGenerator(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min);
-
 }
 
 //2.creare un array con 5 numeri creati con random 
 
 let randomNumbers = [];
 let matchingNumbers = [];
+let userNumbers = [];
 
 while (randomNumbers.length < 5) {
     let newNumber = randomGenerator(1, 100);
@@ -18,18 +18,13 @@ while (randomNumbers.length < 5) {
     randomNumbers.push(newNumber);
 }
 
-//console.log(randomNumbers);
-
-
 //3.creare un alert che espone i 5 numeri.
 
 alert(`${randomNumbers[0]}, ${randomNumbers[1]}, ${randomNumbers[2]}, ${randomNumbers[3]}, ${randomNumbers[4]}`);
 
 //4.impostare un timeout: dopo  30 secondi eseguire il prompt.
-let userNumbers = [];
 
-
-setTimeout(createPrompt, 5000, userNumbers);
+setTimeout(createPrompt, 3000, userNumbers);
 
 //5. creare dei prompt dove l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente
 
@@ -43,26 +38,29 @@ function createPrompt(promptArray) {
         // promptArray.push(prompt(`Inserisci il ${i} numero su 5 di quelli visualizzati`)); 
 
     }
-    findMatchingElement(randomNumbers, promptArray, matchingNumbers);
+    findMatchingElements(randomNumbers, promptArray, matchingNumbers);
 
-    let numberOfMatching = matchingNumbers.length;
+    if (matchingNumbers.length === 0) {
+        alert("Non hai individuato neanche un numero!");
+    } else {
+        //6b
+        let numberOfMatching = matchingNumbers.length;
+        //6a
+        let stringOfMatching = matchingNumbers.join();
 
-        alert(`hai individuato ${numberOfMatching} numeri. `)
+        alert(`Hai individuato ${numberOfMatching} numeri! I numeri indovinati sono: ${stringOfMatching}`)
 
+    }
 
 }
 
 //console.log(userNumbers);
 
-//6. confrontare i 2 array di numeri : userNumbers e randomNumbers.
-//6a. quali sono i numeri uguali --> creando un array che contiene i numeri in comune (matchingNumber)
-//6b. quanti numeri uguali hanno --> contando il numero di valori che "matchingNumber" ha.
+//6. creare una funzione che confronta i 2 array di numeri : userNumbers e randomNumbers.
+//6a.individuare quali sono i numeri uguali --> creando un array che contiene i numeri in comune (matchingNumber)
+//6b.individuare quanti numeri uguali hanno --> contando il numero di valori che "matchingNumber" ha.
 
-
-
-
-function findMatchingElement(arr1, arr2, arr3) {
-
+function findMatchingElements(arr1, arr2, arr3) {
 
     for (let i = 0; i < arr1.length; ++i) {
 
